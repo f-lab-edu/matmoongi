6fe05f842a36c34a4be4a8b7442b9d4024ea4c74
shjung <gcjung777@gmail.com>
2023-07-04 14:55:44 +0900

    loginScreen

---
+++ app/src/main/java/com/matmoongi/screens/LoginScreen.kt
@@ -0,0 +1,57 @@
+package com.matmoongi.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.matmoongi.R
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 120.dp, bottom = 120.dp),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.signature),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth().height(180.dp),
+        )
+        Text(
+            text = stringResource(R.string.matmoongi),
+            modifier = Modifier.fillMaxWidth(),
+            fontFamily = FontFamily(Font(R.font.bagel_fat_one_regular)),
+            fontSize = 64.sp,
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = stringResource(R.string.catchPhrase),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_naver_login),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth().padding(top = 120.dp),
+        )
+        Text(
+            text = stringResource(R.string.skipLogin),
+            modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
